ca9dd91cd22a40e3175f9dc8a91f3dbafd36eb65
Scott Campbell <stc9486@gmail.com>
2020-02-03 11:55:59 -0800

    intial meta-data structure

---
+++ data/data.js
@@ -0,0 +1,23 @@
+const water = {
+    id: 'water',
+    title: 'Water',
+    image: '',
+    description: `Store at least 1 gallon of water per day for each person and each pet. You should consider storing more water than this for hot climates, for pregnant women, and for persons who are sick.`,
+    options: [{
+        id: 'waterOne',
+        description: `Water storage cubes`,
+    }, {
+        id: 'waterTwo',
+        description: `Water filtration system`,
+    }, {
+        id: 'waterThree',
+        description: `water treatment`,
+    }],
+};
+
+
+
+export const categories = [
+    water,
+
+];
