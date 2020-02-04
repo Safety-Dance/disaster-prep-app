a4926bd0f707aabf10eb99fc9afee3e67771896a
Jenna Goldman <jenna.goldman89@gmail.com>
2020-02-04 11:20:20 -0800

    add comments to render-category.js

--- category-form/render-category.js
+++ category-form/render-category.js
@@ -3,15 +3,16 @@ import { renderOption } from './render-options.js';
 const form = document.querySelector('form');
 
 export default function renderCategory(category) {
+    // put category description text in p tag
     const pTag = document.getElementById('description');
     pTag.textContent = category.overview;
 
+    // for each category option, create a label tag with the option description text, append a checkbox input to the label, and append the label to the form
     for (let i = 0; i < category.options.length; i++) {
         const option = category.option[i];
         const renderedOption = renderOption(option);
         form.appendChild(renderedOption);
     }
-
 
     const button = document.createElement('button');
     button.textContent = 'Continue';
