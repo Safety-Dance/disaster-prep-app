ab5a20fce1a755d7d27579ab1fbf6c10389136b2
joegpdx <joegpdx@gmail.com>
2020-02-03 17:49:04 -0800

    utils

---
+++ common/utils.js
@@ -0,0 +1,22 @@
+export function makeUser(formData) {
+    const user = {
+        name: formData.get('name'),
+        description: formData.get(''),
+        options: formData.get(''),
+        completed: {}
+    };
+
+    return user;
+}
+
+export function saveUser(user) {
+    const json = JSON.stringify(user);
+    localStorage.setItem('user', json);
+}
+
+export function getUser() {
+    const json = localStorage.getItem('user');
+    if (!json) return null;
+    const user = JSON.parse(json);
+    return user;
+}
