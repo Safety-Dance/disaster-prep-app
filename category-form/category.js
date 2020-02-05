fc9bb03e545a946a02d32a92c9236c937a1130d8
janelle mellor <janellemellor@gmail.com>
2020-02-05 13:39:05 -0800

    add progress bar

--- category-form/category.js
+++ category-form/category.js
@@ -6,6 +6,7 @@ const categories = categoriesArray.slice();
 const user = getUser();
 
 const form = document.querySelector('form');
+const progressBar = document.getElementById('progress-bar');
 
 const searchParams = new URLSearchParams(window.location.search);
 const categoryId = searchParams.get('id');
@@ -29,6 +30,8 @@ form.addEventListener('submit', (event) => {
     const nextIndex = currentIndex + 1;
     const nextCategory = categories[nextIndex];
     const nextCategoryId = nextCategory.id;
+
+    progressBar.value++;
 
     if (nextIndex < 6) {
         window.location = `./index.html?id=${nextCategoryId}`;
